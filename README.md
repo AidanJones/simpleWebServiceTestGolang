ae996f37fd73a2365c58ab1b9c76fc5bf9ac89a2
AidanJones <aidanjones@gmail.com>
2016-04-30 16:29:02 +0100

    Initial Code
    Initial code.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # simpleWebServiceTestGolang
 Simple web service which allows users to store and retrieve plain text messages.
+
+
+Develop a simple web service which allows users to store and retrieve plain text messages.
+
+The service should behave as follows:
+
+$ curl $domain/messages/ -d 'my test message to store' 
+{"id":12345}	
+
+$ curl $domain/messages/12345
+my test message to store
+
+# Overview
+Places the message (value) to store into a map with a unique id (key). The key is returned in the form {"id":12345}.	
+
+When the a request for the value comes in, if it exists it will be retrieved, if it does not then there following error is sent. "message Id not found".
+
+
+# How to run:
+
+Download the repo.
+In terminal navigate to the route of downloaded folder
+Type go install
+Run go file from bin dir.
+
+# How to Test:
+From a mac simple paste these commands in to terminal. 
+
+$ curl localhost:8080/messages/ -d 'my test message to store' 
+{"id":12345}
+
+$ curl localhost:8080/messages/12345
---
+++ simpleWebService.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+var messageMap map[string]string
+var umessageid int
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	//TODO split handler in to 2 based on parsing of requies, might be advance MUX.
+
+	var returnMessage string = ""
+
+	//If there is some data sent in.
+	if r.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
+		fmt.Printf("Got input.\n")
+
+		//It appears that the the data could also be taken from the body if the Parse form is not run.
+		fmt.Printf("Request Form values.\n")
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Printf("Error parsing form.\n")
+		}
+		var message string
+		for k, v := range r.PostForm {
+			fmt.Printf("  [%s]: \"%s\"\n", k, v)
+			message = k
+		}
+
+		fmt.Printf("Message sent in " + message + "\n")
+		var messageid string = addToMessageMap(message)
+		fmt.Printf("Message ID " + messageid + "\n")
+		returnMessage = "" + messageid
+		//retun json  object with message id
+
+		w.Header().Set("Content-Type", "application/json")
+
+		//TODO create struct and parse this as json?
+		fmt.Fprintf(w, "{\"id\":"+messageid+"}"+"\n")
+	} else {
+		//Now take the id from the path
+		fmt.Printf("Got request.\n")
+		fmt.Printf("Path " + r.URL.Path + "\n")
+		var messageid string = strings.SplitAfter(r.URL.Path, "/")[2]
+		fmt.Printf("Message ID " + messageid + "\n")
+		var message string = retreiveFromMessageMap(messageid)
+		if message != "" {
+			returnMessage = "" + message
+		} else {
+			returnMessage = "message Id not found"
+		}
+
+		fmt.Fprintf(w, returnMessage+"\n")
+	}
+
+}
+
+func addToMessageMap(value string) string {
+	//TODO Could have uid in a concurency safe manner. Use a UID package?
+	umessageid = umessageid + 1
+	var key string = fmt.Sprintf("%v", umessageid)
+	messageMap[key] = value
+	return key
+}
+
+func retreiveFromMessageMap(key string) string {
+	return messageMap[key]
+}
+
+func main() {
+
+	messageMap = make(map[string]string)
+	umessageid = 12344
+
+	http.HandleFunc("/messages/", handler)
+	http.ListenAndServe(":8080", nil)
+
+}
